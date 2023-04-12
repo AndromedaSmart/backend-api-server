da32789b00ef6b05493ce2f405836c96341113a1
Dmitriy Tikhomirov <uroborosq@ya.ru>
2023-04-12 16:14:25 +0300

    feat: add validation, swagger, logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core"
+import { AppModule } from "./app.module"
+import * as morgan from "morgan"
+import { NestExpressApplication } from "@nestjs/platform-express"
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"
+import { ValidationPipe } from "@nestjs/common"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+		logger: ["error", "warn"]
+	})
+	app.use(morgan("tiny"))
+	const config = new DocumentBuilder()
+		.setTitle("Learning vocab backend API")
+		.setVersion("0.0.1")
+		.build()
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup("swagger", app, document)
+	const port = process.env.PORT || 3000
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true,
+			forbidUnknownValues: true,
+			enableDebugMessages: true
+		}),
+	)
+	
+	console.log(`Listening on port ${port}`)
+	await app.listen(port)
 }
-bootstrap();
+bootstrap() 
